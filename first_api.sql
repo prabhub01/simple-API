7058aa94e77231002fa6fe9a9adfb9bc1abb1bc8
Prabhu Budhathoki <prabhu.byn@gmail.com>
2020-12-08 21:35:23 +0545

    Adding database's sql file

---
+++ first_api.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 08, 2020 at 04:49 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.3.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `first_api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `address`) VALUES
+(1, 'Prabhu', 'prabhu@gmail.com', 'Banepa'),
+(2, 'Manjula', 'manju@gmail.com', 'ktm'),
+(3, 'Alaka', 'alaka@gmail.com', 'pokhara'),
+(4, 'Sanjeev', 'sanjeev@gmail.com', 'bhaktapur'),
+(5, 'Sumo', 'supmo@gmail.com', 'Chovar'),
+(6, 'Aabhusan', 'aabhusan@gmail.com', 'Patan');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
